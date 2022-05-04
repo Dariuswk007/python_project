9fcb302d921ee85d6d2bd0174202632f302a1414
Dariuswk007 <dariuswk@aol.com>
2022-05-03 22:53:36 -0500

    dnd sheet

---
+++ main.py
@@ -0,0 +1,18 @@
+dnd_library = [
+  {
+    'characters': {
+      'priest': 'Marcov',
+      'warrior': 'Draken',
+      'berserker': 'Mileena',
+      'druid': 'Shyvanna',
+    }
+  },
+  {
+    'starting_hp': {
+      'marcov': 100,
+      'draken': 200,
+      'mileena': 180,
+      'shyvanna': 120
+    }
+  }
+]
