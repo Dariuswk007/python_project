eaa31dcaa71944f37a2b6b472686b5e086026b1f
Dariuswk007 <dariuswk@aol.com>
2022-05-04 19:02:06 -0500

    revamped code

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-dnd_library = [
-  {
-    'characters': {
-      'priest': 'Marcov',
-      'warrior': 'Draken',
-      'berserker': 'Mileena',
-      'druid': 'Shyvanna',
-    }
-  },
-  {
-    'starting_hp': {
-      'marcov': 100,
-      'draken': 200,
-      'mileena': 180,
-      'shyvanna': 120
-    }
-  }
+dnd_roles = ['Priest', 'Warrior', 'Berserker', 'Druid']  
+
+characters_hp = [
+  {'Marcov': 100},
+  {'Draken': 200},
+  {'Mileena': 180},
+  {'Shyvanna': 120}
 ]
+
+character_sheet = dict(zip(dnd_roles, characters_hp))
+
+character_intro = f'This is the starting party for the next adventure!\n\n {character_sheet}.\n\n Start your new journey!'
+
+#print(character_sheet)
+
+print(character_intro)
